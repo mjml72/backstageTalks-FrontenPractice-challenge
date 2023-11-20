5f42c87adf10c084242fc80d4295a242727922fa
Miguel Jesús <mjml1972@outlook.es>
2023-11-20 16:18:46 +0000

    code refactoring

--- main.js
+++ main.js
@@ -3,25 +3,25 @@ const body = document.body;
 
 
 window.addEventListener("scroll", () => {
-    if (scrollY >= 0) {
+    if (window.scrollY >= 0) {
         body.style.backgroundColor = colors[0];
     }
-    if (scrollY > window.innerHeight * 0.8) {
+    if (window.scrollY > window.innerHeight * 0.8) {
         body.style.backgroundColor = colors[1];
     }
-    if (scrollY > window.innerHeight * 1.8) {
+    if (window.scrollY > window.innerHeight * 1.8) {
         body.style.backgroundColor = colors[2];
     }
-    if (scrollY > window.innerHeight * 2.8) {
+    if (window.scrollY > window.innerHeight * 2.8) {
         body.style.backgroundColor = colors[3];
     }
-    if (scrollY > window.innerHeight * 3.8) {
+    if (window.scrollY > window.innerHeight * 3.8) {
         body.style.backgroundColor = colors[4];
     }
-    if (scrollY > window.innerHeight * 4.8) {
+    if (window.scrollY > window.innerHeight * 4.8) {
         body.style.backgroundColor = colors[5];
     }
-    if (scrollY > window.innerHeight * 5.8) {
+    if (window.scrollY > window.innerHeight * 5.8) {
         body.style.backgroundColor = colors[6];
     }
 
